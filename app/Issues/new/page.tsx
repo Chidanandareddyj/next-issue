2a37459f800d982761d30450944799f74c4d9167
Jonnala Chidananda Reddy <jlcreddy17@gmail.com>
2025-04-07 00:14:46 +0530

    feat: implement issue creation form with validation and integrate axios for API requests

--- app/Issues/new/page.tsx
+++ app/Issues/new/page.tsx
@@ -3,19 +3,47 @@
 import { TextField,TextArea, Button } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm,Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+    title: string;
+    description: string;
+} // This interface defines the structure of the form data
+// that will be submitted when the form is submitted.
 
 const page = () => {
+    const router=useRouter() // This hook is used to programmatically navigate to different routes in the application.
+    const {register,control,handleSubmit}=useForm<IssueForm>({
+        defaultValues: {
+            title: "",
+            description: "",
+        },
+    }); // This initializes the form with default values for title and description.
+    // The useForm hook from react-hook-form is used to manage the form state and validation.
+
+
     return (
-        <div className='max-w-2xl mx-auto mt-10 space-y-3'>
-            <TextField.Root placeholder="Search the docs…">
+        <form className='max-w-2xl mx-auto mt-10 space-y-3' 
+        onSubmit={handleSubmit(async (data) =>{ 
+            await axios.post("/api/issues",data);
+            router.push("/issues") // This function is called when the form is submitted. It sends a POST request to the /api/issues endpoint with the form data.
+            // After the request is successful, it redirects the user to the Issues page.
+             })}>
+            <TextField.Root placeholder="Search the docs…" {...register("title")}>
                 <TextField.Slot>
                 </TextField.Slot>
             </TextField.Root>
-            <SimpleMDE placeholder="Reply to comment…" />
+            <Controller // This component is used to control the SimpleMDE editor
+                name="description"
+                control={control}
+                render={({field})=> <SimpleMDE placeholder="Reply to comment…"  {...field}/>} //
+            />
             <Button  className='w-full'>
                 Create Issue
             </Button>
-        </div>
+        </form>
     )
 }
 
